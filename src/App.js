b58712c007ed95c7f229cb7ea69dcc0e8c67f200
Akhil Thomas <akt.akku88@gmail.com>
2022-09-18 08:12:59 +0530

    Landing Page updated

--- src/App.js
+++ src/App.js
@@ -1,38 +1,25 @@
 function App() {
   return (
-    <div class="w-full mx-auto font-lexend">
-          <div className="relative w-full h-96 flex justify-between items-center overflow-hidden">
-            <div class="absolute top-56 -left-6 w-48 h-48 rounded-full bg-green-50"></div>
-            {/* <div class="absolute bottom-16 -right-0 w-16 h-16 rounded-full bg-green-50"></div> */}
+    <div className="w-full mx-auto font-lexend">
+        <div className="relative w-full h-[480px] flex justify-between items-center overflow-hidden">
+            <div className="absolute top-80 -left-6 w-52 sm:w-80 h-52 sm:h-80 rounded-full bg-green-50"></div>
+            <div className="absolute bottom-80 -right-6 w-52 sm:w-80 h-52 sm:h-80 rounded-full bg-green-50"></div>
             <div className="p-4 z-10">
-                <div className="text-green-900">
-                    <h1 class="text-7xl font-semibold">Hello</h1>
-                    <h1 class="text-7xl font-semibold">I'am <span class="text-green-500">Akhil Thomas</span></h1> 
+                <div className="text-7xl font-semibold text-green-900">
+                    <h1 className="">Hello</h1>
+                    <h1 className="">I'am <span className="text-green-500">Akhil Thomas</span></h1> 
                 </div>
-            </div>
-        </div>
-          
-          
-        <div class="relative max-w-7xl h-[480px] mx-auto bg-white flex justify-between items-center overflow-hidden">
-            <div class="absolute top-44 -left-10 w-96 h-96 rounded-full bg-green-50"></div>
-            <div class="absolute bottom-44 -right-20 w-96 h-96 rounded-full bg-green-50"></div>
-            <div class="p-2 z-10">
-                <div class="my-5 flex flex-col justify-start items-start text-green-900">
-                    <h1 class="text-7xl font-semibold">Hello</h1>
-                    <h1 class="text-7xl font-semibold">I'am <span class="text-7xl font-semibold text-green-500">Akhil Thomas</span></h1>
-                    
-                </div>
-                {/* <h3 class="my-4 text-xl font-medium text-gray-600">Full Stack Developer</h3> */}
-                <div class="relative">
-                    <p class="my-4 text-base leading-tight text-green-800">Software Developer at Tata Consultancy Services(TCS)</p>
-                    <span class="absolute -bottom-1 right-64 w-[248px] h-[2px] rounded-lg bg-green-900"></span>
-                </div>
+                <div className="p">
+                      <p className="my-4 text-sm leading-tight text-green-800">Software Developer at <span className="text-sm font-medium text-green-900">Tata Consultancy Services (TCS)</span></p>
+                  </div>
+                <div className="">
                 <button class="my-4 px-6 py-4 bg-green-500 text-white rounded-lg flex">
                     <span class="mr-2">Contact me </span>
                     <svg xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 24 24" stroke-width="1.5" stroke="currentColor" class="w-6 h-6">
                         <path stroke-linecap="round" stroke-linejoin="round" d="M13.5 4.5L21 12m0 0l-7.5 7.5M21 12H3" />
                     </svg>
                 </button>
+                </div>
             </div>
         </div>
     </div>
